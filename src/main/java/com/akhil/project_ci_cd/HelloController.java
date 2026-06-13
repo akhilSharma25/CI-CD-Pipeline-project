1e39fa2409f379ce356d10f39acc2f0ea6976ca5
AKhil Sharma <akhilkatyan25b@gmail.com>
2026-06-13 21:11:42 +0530

    upload first

---
+++ src/main/java/com/akhil/project_ci_cd/HelloController.java
@@ -0,0 +1,13 @@
+package com.akhil.project_ci_cd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getData(){
+        return "Hello Developers...";
+    }
+}
---
+++ src/test/java/com/akhil/project_ci_cd/ProjectCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhil.project_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
